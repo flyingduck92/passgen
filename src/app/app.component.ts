aa473113dfbe9ab9d6f639a6b8a88f38f671e8e0
Sekti <14885417+flyingduck92@users.noreply.github.com>
2023-09-26 08:55:38 +0700

    Angular Passgen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password';
+  
+  length = 0
+  password = ''
+  isLetters = false
+  isNumbers = false
+  isSymbols = false
+
+  onChangeLength(event: Event) {
+    let {value} = event.target as HTMLInputElement    
+    const number = parseInt(value)
+
+    if(!isNaN(number)) {
+      this.length = number
+    }
+
+    console.log('Length: ', this.length)
+  }
+
+  useLetters() {
+    this.isLetters = !this.isLetters
+  }
+  useNumbers() {
+    this.isNumbers = !this.isNumbers
+  }
+  useSymbols() {
+    this.isSymbols = !this.isSymbols
+  }
+
+  submit() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    const numbers = '123456789'
+    const symbols = '!@#$%^&*()'
+
+    let passCharacters = ''
+    if(this.isLetters) {
+      passCharacters += letters
+    }
+    if(this.isNumbers) {
+      passCharacters += numbers
+    }
+    if(this.isSymbols) {
+      passCharacters += symbols
+    }
+
+    let generatePassword = ''
+    for(let i=0;i<this.length;i++) {
+      const index = Math.floor(Math.random() * passCharacters.length)
+      generatePassword += passCharacters[index]
+    }
+
+    this.password = generatePassword
+
+  }
+
 }
